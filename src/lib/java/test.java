93022c584dbf6a3f7f4658a1ac983fe3611e24f3
00Danii <jdaniel.om97@gmail.com>
2025-10-06 16:42:05 -0600

    feat: codigo de ejemplo de java

---
+++ src/lib/java/test.java
@@ -0,0 +1,35 @@
+public class Main {
+  public static void main(String[] args) {
+    // comentarios
+    /* 
+      comentario de bloque
+      1
+      2
+    */
+
+    // DECLARACION DE VARIABLES
+    int contador = 0;
+    double promedio = 3.5;
+    boolean activo = true;
+    String mensaje = "mensaje";
+
+    // PRINTLN
+    System.out.println("Hola");
+
+    // PRINT
+    System.out.print("Adios");
+
+    // IF ELSE-IF ELSE
+    int x = 0;
+    if(x >= 8) {
+      System.out.println("if");
+    } 
+    else if (x == 5) {
+      System.out.println("else if");
+    } 
+    else {
+      System.out.println("else");
+    }
+    
+  }
+}
