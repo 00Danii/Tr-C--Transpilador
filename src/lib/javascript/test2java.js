ead9484a05ba4624ab301a65575c46aaee594eb2
00Danii <jdaniel.om97@gmail.com>
2025-10-23 21:39:48 -0600

    refactor: test2java.js - Agregar declaración explicita de variables

--- src/lib/javascript/test2java.js
+++ src/lib/javascript/test2java.js
@@ -1,23 +1,23 @@
 // INFERENCIA DE TIPOS
 // INT
-int1 = 5
-int2 = -3
+let int1 = 5
+let int2 = -3
 
 // DOUBLE
-d1 = 0.900931
-d2 = -3.1415926535
+let d1 = 0.900931
+let d2 = -3.1415926535
 
 // BOOLEAN
-b1 = true
-b2 = false
+let b1 = true
+let b2 = false
 
 // STRING
-s1 = "Hola"
-s2 = "Adios"
+let s1 = "Hola"
+let s2 = "Adios"
 
 // CHAR
-c1 = 'a'
-c2 = 'b'
+let c1 = 'a'
+let c2 = 'b'
 
 // IF ELSE IF ELSE
 let contador = 0;
@@ -27,4 +27,11 @@ if (contador === 0) {
   console.log("Contador positivo");
 } else {
   console.log("Contador negativo");
+}
+
+// WHILE
+let contador2 = 0
+while (contador2 < 3) {
+  console.log(contador2);
+  contador2++;
 }
