49723003cd90129f94aed5ab681f3b11e31ecae7
00Danii <jdaniel.om97@gmail.com>
2025-10-23 20:58:45 -0600

    feat: test de javascript a java

---
+++ src/lib/javascript/test2java.js
@@ -0,0 +1,20 @@
+// INFERENCIA DE TIPOS
+// INT
+int1 = 5
+int2 = -3
+
+// DOUBLE
+d1 = 0.900931
+d2 = -3.1415926535
+
+// BOOLEAN
+b1 = true
+b2 = false
+
+// STRING
+s1 = "Hola"
+s2 = "Adios"
+
+// CHAR
+c1 = 'a'
+c2 = 'bw'
